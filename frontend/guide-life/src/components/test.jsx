bd99498ad754b48103f385ac9f5ad882c2beaa23
Oriol Roura <oriol.roura@gmail.com>
2025-03-15 13:09:58 +0100

    front end css configuration

--- frontend/guide-life/src/components/test.jsx
+++ frontend/guide-life/src/components/test.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Test = () => {
   return (
-    <div className="bg-blue-500 text-white p-4">
+    <div className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-blue-500 text-white p-4">
       This is a Tailwind CSS styled component.
     </div>
   );
